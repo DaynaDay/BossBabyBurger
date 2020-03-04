9e083cf35e9dd3e1dfd058b6d54dd6b33aa949af
Dayna Knight <dknighted11@gmail.com>
2020-03-04 13:08:27 -0500

    finished controller file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,68 +1,64 @@
 const connection = require('../config/connection');
 
-// GET ALL CATS
-const getCats = () => {
+
+const getBurgers = () => {
   return new Promise((resolve, reject) => {
-    connection.query('SELECT * FROM cats', (err, catdata) => {
+    connection.query('SELECT * FROM burgers', (err, burgerdata) => {
       if (err) {
         console.log(err);
-        // THIS WILL GO TO PROMISE'S .catch()
+      
         return reject(err);
       }
 
-      // THIS WILL GO TO PROMISE'S .then()
-      resolve(catdata);
+     
+      resolve(burgerdata);
     });
   });
 };
 
-// create a cat
-/* accepts object parameter => {cat_name: "Mr. Mustaphales" } */
-const createCat = catObj => {
+const createBurger = burgerObj => {
   return new Promise((resolve, reject) => {
-    connection.query('INSERT INTO cats SET ?', catObj, (err, catdata) => {
+    connection.query('INSERT INTO burgers SET ?', burgerObj, (err, burgerdata) => {
       if (err) {
         console.log(err);
-        // THIS WILL GO TO PROMISE'S .catch()
+        
         return reject(err);
       }
-      // THIS WILL GO TO PROMISE'S .then()
-      resolve(catdata);
+      
+      resolve(burgerdata);
     });
   });
 };
 
-// UPDATE A CAT'S ADOPTION STATUS
-// catObj => { adopted: true } OR {adopted: false}
-const updateCat = (catObj, catId) => {
+const updateBurger = (burgerObj, burgerId) => {
   return new Promise((resolve, reject) => {
-    connection.query('UPDATE cats SET ? WHERE id = ?', [catObj, catId], (err, catdata) => {
+    connection.query('UPDATE burgers SET ? WHERE id = ?', [burgerObj, burgerId], (err, burgerdata) => {
       if (err) {
         console.log(err);
         return reject(err);
-      } else if (catdata.affectedRows === 0) {
-        return resolve({ message: "Couldn't find a cat with that id!", code: 404 });
+      } else if (burgerdata.affectedRows === 0) {
+        return resolve({ message: "Couldn't find a Burger with that id!", code: 404 });
       }
 
-      resolve({ message: 'Cat updated successfully!', code: 200 });
+      resolve({ message: 'Burger updated successfully!', code: 200 });
     });
   });
 };
 
 // DELETE A CAT
-const deleteCat = catId => {
+const deleteBurger = catId => {
   return new Promise((resolve, reject) => {
-    connection.query('DELETE FROM cats WHERE id = ?', [catId], (err, catdata) => {
+    connection.query('DELETE FROM burgers WHERE id = ?', [burgerId], (err, burgerdata) => {
       if (err) {
         console.log(err);
         return reject(err);
-      } else if (catdata.affectedRows === 0) {
-        return resolve({ message: "Couldn't find a cat with that id!", code: 404 });
+      } else if (burgerdata.affectedRows === 0) {
+        return resolve({ message: "Couldn't find a burger with that id!", code: 404 });
       }
 
-      resolve({ message: 'Cat deleted successfully!', code: 200 });
+      resolve({ message: 'Burger deleted successfully!', code: 200 });
     });
   });
 };
 
-module.exports = { getCats, createCat, updateCat, deleteCat };
+module.exports = { getBurger, createBurger, updateBurger, deleteBurger };
