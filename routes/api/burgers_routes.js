0c186a5c71a5b4d12f0cd3da7101e5c9c818c0b5
Dayna Knight <dknighted11@gmail.com>
2020-03-06 12:20:43 -0500

    finished views and api routes.

---
+++ routes/api/burgers_routes.js
@@ -0,0 +1,53 @@
+const router = require('express').Router();
+
+const { getBurger, createBurger, updateBurger, deleteBurger } = require('../../controllers/burgers_controller');
+
+router.get('/burgers', (req, res) => {
+  getBurger()
+    .then(burgerdata => {
+      res.status(200).json(burgerdata);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+router.post('/burgers', (req, res) => {
+  
+  createBurger(req.body)
+    .then(burgerdata => {
+      res.status(200).json(burgerdata);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+router.put('/burgers/:id', (req, res) => {
+  updateBurger(req.body, req.params.id)
+    .then(burgerdata => {
+      if (burgerdata.code === 404) {
+        return res.status(404).json(burgerdata);
+      }
+      res.status(200).json(burgerdata);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+router.delete('/burgers/:id', (req, res) => {
+  deleteBurger(req.params.id)
+    .then(burgerdata => {
+      if (burgerdata.code === 404) {
+        return res.status(404).json(burgerdata);
+      }
+
+      res.status(200).json(burgerdata);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
