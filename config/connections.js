7881762b12a5f15696d273e97f44fbf60fd87726
Dayna Knight <dknighted11@gmail.com>
2020-03-06 20:47:22 -0500

    worked on connections file

--- config/connections.js
+++ config/connections.js
@@ -10,12 +10,12 @@ var connection = mysql.createConnection({
   database: DB_NAME
 });
 
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
 module.exports = connection;
