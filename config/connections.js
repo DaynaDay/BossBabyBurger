6b820f1a9c66b4710b0487eeb28399d8a13d50d6
Dayna Knight <dknighted11@gmail.com>
2020-03-06 20:39:37 -0500

    created publics folder and added front code.

--- config/connections.js
+++ config/connections.js
@@ -1,5 +1,7 @@
 var mysql = require("mysql");
 
+require('dotenv').config();
+
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -8,12 +10,12 @@ var connection = mysql.createConnection({
   database: DB_NAME
 });
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
 module.exports = connection;
---
+++ routes/html/html_routes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+
+
+const { getBurgers } = require('../../controllers/burgers_controller');
+
+
+router.get('/', (req, res) => {
+  
+  getBurgers()
+    .then(burgerdata => {
+      res.render('home', { burgers: burgerdata });
+    })
+    .catch(err => {
+      res.status(500).end();
+    });
+});
+
+module.exports = router;
