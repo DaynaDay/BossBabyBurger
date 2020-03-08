9d9c306851bc3e05c1e74a933cdb8fa72d2895fa
Dayna Knight <dknighted11@gmail.com>
2020-03-07 19:47:04 -0500

    pushing to heroku

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,17 @@ var mysql = require("mysql");
 
 require('dotenv').config();
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: 'root',
-  password: 'KnightlyNews93',
-  database: 'burger_db'
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: 'root',
+    password: 'KnightlyNews93',
+    database: 'burger_db'
+  });
+};
 
 module.exports = connection;
